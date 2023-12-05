494567fb638e63364965702e5de0880bd97ec8b8
SongyuanSui <SongyuanSui@users.noreply.github.com>
2023-12-05 00:45:20 +0000

    deploy: 5bef9dc26c0c999d8cdee664393d9fe7607b7f0e

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1701702118';
+const cacheName = 'chirpy-1701737120';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
