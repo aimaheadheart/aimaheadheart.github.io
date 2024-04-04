c0f86c66fe1da9b0500cf3cbb25d5974dbc3ee11
aimaheadheart <aimaheadheart@users.noreply.github.com>
2024-04-04 18:42:57 +0000

    deploy: d81df75f0dd2734f55cb53ae664b2c1a9c2a636c

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1712255517';
+const cacheName = 'chirpy-1712256176';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
